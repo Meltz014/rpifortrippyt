a11650c9d3dcf56e07db71e36984c6f1b8c699e2
David Melton <dmelton@qti.qualcomm.com>
2018-09-07 11:53:16 -0600

    fix last commit

--- webiface.py
+++ webiface.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import AquariumLights
-from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify, redirect, url_for
 from sqlalchemy.orm import sessionmaker
 from tabledef import *
 import logging
@@ -31,21 +31,27 @@ class InvalidUsage(Exception):
         rv['message'] = self.message
         return rv
 
-def require_login(func):
-    """
-    Decorator to require login
-    """
-    def login_wrapper(*args, **kwargs):
-        if not session.get('logged_in'):
-            return render_template('login.html')
-        else:
-            return func(*args, **kwargs)
-
-    return login_wrapper
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+ 
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return redirect(url_for('main'))
 
 @app.route("/")
-@require_login
 def main():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+
     app.logger.debug('hello')
     templateData = {
         'config_state': lights_control.get_config_state()
@@ -55,8 +61,10 @@ def main():
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
 @app.route("/<key>", methods=["GET", "POST"])
-@require_login
 def handleattr(key):
+    if not session.get('logged_in'):
+        return render_template('login.html')
+
     value = ''
 
     if key == 'favicon.ico':
@@ -84,4 +92,5 @@ def logout():
 
 if __name__ == "__main__":
     app.debug = True
+    app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0', port=443, debug=True, ssl_context='adhoc')
