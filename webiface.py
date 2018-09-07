12a3d67f7db3c2040cc750e6f1792d12e907437c
David Melton <dmelton@qti.qualcomm.com>
2018-09-07 11:52:37 -0600

    Add auth to webiface

--- webiface.py
+++ webiface.py
@@ -1,12 +1,16 @@
 import sys
 import os
 import AquariumLights
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
 import logging
 
 logger = logging.getLogger("AquariumLights")
 
 lights_control = AquariumLights.LightControl()
+
+engine = create_engine('sqlite:///tutorial.db', echo=True)
 
 app = Flask('Flask')
 app.logger.setLevel(logging.DEBUG)
@@ -27,7 +31,20 @@ class InvalidUsage(Exception):
         rv['message'] = self.message
         return rv
 
+def require_login(func):
+    """
+    Decorator to require login
+    """
+    def login_wrapper(*args, **kwargs):
+        if not session.get('logged_in'):
+            return render_template('login.html')
+        else:
+            return func(*args, **kwargs)
+
+    return login_wrapper
+
 @app.route("/")
+@require_login
 def main():
     app.logger.debug('hello')
     templateData = {
@@ -38,6 +55,7 @@ def main():
 
 # The function below is executed when someone requests a URL with the pin number and action in it:
 @app.route("/<key>", methods=["GET", "POST"])
+@require_login
 def handleattr(key):
     value = ''
 
@@ -59,6 +77,11 @@ def handleattr(key):
         setattr(lights_control, key, new_val)
         return jsonify('success')
 
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return main()
+
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=443, debug=True, ssl_context='adhoc')
